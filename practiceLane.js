7ac495e5c3db06f926ab39781eeb4ebe8410abb9
llamouth <llamouth@pursuit.org>
2024-01-09 13:33:13 -0500

    Saved new commit

--- practiceLane.js
+++ practiceLane.js
@@ -1,21 +1,36 @@
-const { get } = require("http");
+const { availableParallelism } = require("os");
 const exampleSongData = require("./data/songs");
 
-function mapArtistsToSongs(songs) {
-    const artists = songs.map(song => song.artist)
+function findAlbumWithLongestAverageRuntime(songs) {
+    const albums = songs.map(song => song.album)
     const mappedObject = {};
-    for(const artist of artists) {
-        const arr = []
-        for(const song of songs) {
-            if(artist === song.artist) {
-                arr.push(song.title)
-            }
+    for(const album of albums) {
+      const arr = []
+      for(const song of songs) {
+        if(album === song.album) {
+          arr.push(song.runtimeInSeconds)
         }
-        if(mappedObject.hasOwnProperty(artist)){
-            continue;
-        }
-        mappedObject[artist] = arr
+      }
+      if(mappedObject.hasOwnProperty(album)){
+        continue;
+      }
+      mappedObject[album] = arr
     }
-    return mappedObject
+    for(const album in mappedObject){
+        let average = 0
+        let songAmount = 0
+        for(const time of mappedObject[album]) {
+            average += time
+            songAmount++ 
+        }
+        mappedObject[album] = Math.round(average / songAmount)
+    }
+    let longest = 0
+    for(const album in mappedObject){
+        if(mappedObject[album] > longest){
+            longest = mappedObject[album]
+        }
+    }
+    return longest
 }
-console.log(mapArtistsToSongs(exampleSongData))
+console.log(findAlbumWithLongestAverageRuntime(exampleSongData))
